dd8cd8c541314955dc6bb6acb9c9a52fa2d4330f
Daniel Romero Ávila <v-davila@youngliving.com>
2024-12-13 14:35:22 -0400

    App update task success

---
+++ Angular18AspNetCore8.App.Tests/CommandUpdateTasksTests.cs
@@ -0,0 +1,66 @@
+﻿using Angular18AspNetCore8.App.Commands.UpdateTask;
+using Angular18AspNetCore8.App.Common;
+using Angular18AspNetCore8.App.Queries.GetAllTasks;
+using Angular18AspNetCore8.Core.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace Angular18AspNetCore8.App.Tests;
+
+public class CommandUpdateTasksTests
+{
+  readonly Mock<ITodoTasksRepository> mockTodoTaskRepository;
+  readonly CommandUpdateTaskHandler testUpdateTaskHandler;
+  public CommandUpdateTasksTests()
+  {
+    mockTodoTaskRepository = new Mock<ITodoTasksRepository>();
+    testUpdateTaskHandler = new CommandUpdateTaskHandler(mockTodoTaskRepository.Object);
+  }
+  [Theory]
+  [InlineData(true)]
+  public async Task UpdateTaskSuccess(bool givenUpdateDatetimeNull)
+  {
+    //Arrange
+    var givenTodoTaskStatus = TodoTaskStatus.ToDo;
+    var givenUpdateDatetime = DateTimeOffset.Now;
+    var givenExistingDatetime = DateTimeOffset.Now.AddDays(-1);
+    var givenItemToUpdate = new ItemResultModel
+    {
+      Id = 1,
+      Description = "Some description",
+      DueDate = givenUpdateDatetimeNull ? "" : $"{givenUpdateDatetime:D}",
+      Status = TodoTaskStatusNames.Format[givenTodoTaskStatus],
+    };
+    var givenCommandUpdateTask = new CommandUpdateTask
+    {
+      Item = givenItemToUpdate
+    };
+    var existingTodoTask = new TodoTask
+    {
+      Id = givenCommandUpdateTask.Item.Id,
+      Description = "Existing description",
+      Duedate = givenExistingDatetime,
+      Status = TodoTaskStatus.InProgress
+    };
+
+    mockTodoTaskRepository.Setup(x => x.GetByIds(It.IsAny<IList<int>>())).ReturnsAsync([existingTodoTask]);
+    mockTodoTaskRepository.Setup(x => x.SaveChanges());
+
+    var expectedResult = new CommandUpdateTaskResult
+    {
+      HasValidationErrors = false,
+      Item = new ItemResultModel
+      {
+        Id = givenItemToUpdate.Id,
+        Description = givenItemToUpdate.Description,
+        DueDate = string.IsNullOrEmpty(givenItemToUpdate.DueDate) ? "" : givenItemToUpdate.DueDate,
+        Status = givenItemToUpdate.Status
+      }
+    };
+    //Act
+    var actualResult = await testUpdateTaskHandler.Execute(givenCommandUpdateTask);
+    //Assert
+    mockTodoTaskRepository.VerifyAll();
+    actualResult.Should().BeEquivalentTo(expectedResult);
+  }
+}
--- Angular18AspNetCore8.App/Common/ITodoTasksRepository.cs
+++ Angular18AspNetCore8.App/Common/ITodoTasksRepository.cs
@@ -6,5 +6,7 @@ namespace Angular18AspNetCore8.App.Common
   {
     Task<TodoTask> AddNew(string description, DateTimeOffset? dueDate, TodoTaskStatus status);
     Task<List<TodoTask>> GetAll();
+    Task<List<TodoTask>> GetByIds(IList<int> ids);
+    Task<TodoTask> SaveChanges();
   }
 }
