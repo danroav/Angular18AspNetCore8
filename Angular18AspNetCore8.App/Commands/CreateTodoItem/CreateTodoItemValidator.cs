c6f9b0ed8ec36355ce27b512cff535bd7b828fc3
Daniel Romero Ávila <v-davila@youngliving.com>
2024-12-16 18:59:10 -0400

    Fixes validations keys

--- Angular18AspNetCore8.App/Commands/CreateTodoItem/CreateTodoItemValidator.cs
+++ Angular18AspNetCore8.App/Commands/CreateTodoItem/CreateTodoItemValidator.cs
@@ -7,12 +7,12 @@ public class CreateTodoItemValidator : AbstractValidator<CreateTodoItem>
 {
   public CreateTodoItemValidator()
   {
-    RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
-    RuleFor(x => x.Description).MaximumLength(255).WithMessage("Description should not exceed 255 chars");
+    RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required").WithName("description");
+    RuleFor(x => x.Description).MaximumLength(255).WithMessage("Description should not exceed 255 chars").WithName("description");
     When(x => x.DueDate.HasValue, () =>
     {
-      RuleFor(x => x.DueDate).GreaterThan(DateTimeOffset.Now).WithMessage("Due Date should be in the future");
+      RuleFor(x => x.DueDate).GreaterThan(DateTimeOffset.Now).WithMessage("Due Date should be in the future").WithName("dueDate");
     });
-    RuleFor(x => x.Status).Must(status => TodoItemStatusNames.Parse.ContainsKey(status) && TodoItemStatusNames.Parse[status] != TodoItemStatus.Overdue).WithMessage("Status should be valid");
+    RuleFor(x => x.Status).Must(status => TodoItemStatusNames.Parse.ContainsKey(status) && TodoItemStatusNames.Parse[status] != TodoItemStatus.Overdue).WithMessage("Status should be valid").WithName("status");
   }
 }
--- Angular18AspNetCore8.App/Commands/UpdateTodoItem/UpdateTodoItemValidator.cs
+++ Angular18AspNetCore8.App/Commands/UpdateTodoItem/UpdateTodoItemValidator.cs
@@ -6,14 +6,14 @@ public class UpdateTodoItemValidator : AbstractValidator<UpdateTodoItem>
 {
   public UpdateTodoItemValidator()
   {
-    RuleFor(x => x.Item.Description).NotEmpty().WithMessage("Description is required");
-    RuleFor(x => x.Item.Description).MaximumLength(255).WithMessage("Description should not exceed 255 chars");
+    RuleFor(x => x.Item.Description).NotEmpty().WithMessage("Description is required").WithName("description");
+    RuleFor(x => x.Item.Description).MaximumLength(255).WithMessage("Description should not exceed 255 chars").WithName("description");
     When(x => x.Item.DueDate.HasValue, () =>
     {
       var now = DateTimeOffset.Now;
-      RuleFor(x => x.Item.DueDate).GreaterThan(now).WithMessage("Due Date should be in the future").WithName("Item.DueDate");
+      RuleFor(x => x.Item.DueDate).GreaterThan(now).WithMessage("Due Date should be in the future").WithName("dueDate");
     });
-    RuleFor(x => x.Item.Status).Must(status => TodoItemStatusNames.Parse.ContainsKey(status) && TodoItemStatusNames.Parse[status] != TodoItemStatus.Overdue).WithMessage("Status should be valid");
+    RuleFor(x => x.Item.Status).Must(status => TodoItemStatusNames.Parse.ContainsKey(status) && TodoItemStatusNames.Parse[status] != TodoItemStatus.Overdue).WithMessage("Status should be valid").WithName("status");
 
   }
 }
